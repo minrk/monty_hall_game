5b7f2b8c12bae54a9eb781dd8433ec739ee40754
jorgensd <dokken92@gmail.com>
2023-11-14 21:40:26 +0100

    Add `__all__` and 3.8 test

--- src/monty_hall_game/__init__.py
+++ src/monty_hall_game/__init__.py
@@ -1,4 +1,4 @@
 from .monty_hall_game import MontyHallGame
 from .game_exceptions import InvalidGameInput
 
-# __all__ = ["MontyHallGame", "InvalidGameInput"]
+__all__ = ["MontyHallGame", "InvalidGameInput"]
